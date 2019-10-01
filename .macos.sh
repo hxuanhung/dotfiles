b0fec25d9b7c6961c15702145059e338d101bec8
Hung Hoang <hxuanhung@gmail.com>
2019-10-01 16:12:23 +0100

    .macos.sh reduce dock time hide and show

--- .macos.sh
+++ .macos.sh
@@ -25,4 +25,9 @@ sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.serve
 # Disable press-and-hold for keys in favor of key repeat
 defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 
+# I autohide my dock to maximize screen space and hide those distracting red dots on messaging app icons, 
+# but the default show and hide is too slow. Speed it up:
+defaults write com.apple.dock autohide-time-modifier -float 0.12;killall Dock
+defaults write com.apple.Dock autohide-delay -float 0; killall Dock
+
 echo "Done. Note that some of these changes require a logout/restart to take effect."
